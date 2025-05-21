9cbfe9f1c85e118746fe516813ab5f40cc601562
gehlotnikhil <gehlotnikhil38@gmail.com>
2025-05-21 07:13:42 +0530

    updated

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-const API_BASE = "http://localhost:8000/api";
-// const API_BASE = "https://100x-bank-server.vercel.app/api";
+// const API_BASE = "http://localhost:8000/api";
+const API_BASE = "https://100x-bank-server.vercel.app/api";
 const api = axios.create({
   baseURL: API_BASE,
 });
